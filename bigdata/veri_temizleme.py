52b0469ee80a5c535b3045e62acad88a1e166d8e
baglamishelin <baglamishelin34@gmail.com>
2025-08-08 09:04:16 +0300

    Update veri_temizleme.py

--- bigdata/veri_temizleme.py
+++ bigdata/veri_temizleme.py
@@ -1,19 +1,20 @@
 import json
 import re
 
-# JSON dosyasını yükle
+
 with open("C:/Users/lenovo/OneDrive/Resimler/Masaüstü/veriler/film_yorumlari.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 def clean_text(text):
-    text = re.sub(r'<.*?>', '', text)  # HTML etiketlerini kaldır
-    text = re.sub(r'\W', ' ', text)  # Özel karakterleri kaldır
+    text = re.sub(r'<.*?>', '', text) 
+    text = re.sub(r'\W', ' ', text) 
     return text.lower()
 
-# Temizlenmiş veriyi kaydet
+
 cleaned_reviews = [{"film_id": d["film_id"], "author": d["author"], "content": clean_text(d["content"])} for d in data]
 
 with open("C:/Users/lenovo/OneDrive/Resimler/Masaüstü/veriler/temizlenmis_yorumlar.json", "w", encoding="utf-8") as f:
     json.dump(cleaned_reviews, f, ensure_ascii=False, indent=4)
 
 print("Veri başarıyla temizlendi ve yeni dosyaya kaydedildi!")
+
