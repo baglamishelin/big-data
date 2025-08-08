671e508c93d6fd2e2683cb519d59b023228b6d0c
baglamishelin <baglamishelin34@gmail.com>
2025-08-08 09:21:42 +0300

    Update etiketlenmis_yorumlarline.py

--- bigdata/etiketlenmis_yorumlarline.py
+++ bigdata/etiketlenmis_yorumlarline.py
@@ -1,10 +1,11 @@
 import json
 
-# Dosyayı yükle (dizi şeklinde)
+
 with open("C:/Users/lenovo/OneDrive/Resimler/Masaüstü/veriler/etiketlenmis_yorumlar.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
-# Line-delimited JSON olarak kaydet
+
 with open("C:/Users/lenovo/OneDrive/Resimler/Masaüstü/veriler/etiketlenmis_yorumlar_line.json", "w", encoding="utf-8") as f:
     for entry in data:
         f.write(json.dumps(entry, ensure_ascii=False) + "\n")
+
