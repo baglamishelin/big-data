c3204cbef9f8c4cbcf0d80722dfc55f9ccf2528d
baglamishelin <baglamishelin34@gmail.com>
2025-08-08 09:22:06 +0300

    Update etiketlenmis_veri.py

--- bigdata/etiketlenmis_veri.py
+++ bigdata/etiketlenmis_veri.py
@@ -1,7 +1,7 @@
 import json
 from textblob import TextBlob
 
-# JSON dosyasını yükle
+
 with open("C:/Users/lenovo/OneDrive/Resimler/Masaüstü/veriler/kelime_kokleri.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
@@ -9,10 +9,11 @@ def get_sentiment(text):
     sentiment_score = TextBlob(text).sentiment.polarity
     return "İyi" if sentiment_score > 0 else "Kötü"
 
-# Etiketlenmiş veriyi oluştur
+
 labeled_reviews = [{"film_id": d["film_id"], "author": d["author"], "content": d["content"], "sentiment": get_sentiment(d["content"])} for d in data]
 
 with open("C:/Users/lenovo/OneDrive/Resimler/Masaüstü/veriler/etiketlenmis_yorumlar.json", "w", encoding="utf-8") as f:
     json.dump(labeled_reviews, f, ensure_ascii=False, indent=4)
 
 print("Yorumlar başarıyla etiketlendi ve yeni dosyaya kaydedildi!")
+
